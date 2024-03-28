31acb7eb4d5977000bb364058fd3124c212e8940
abroraxmedov04 <abrordevaxmedov@gmail.com>
2024-03-29 01:03:31 +0200

    header-complete

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
-function formatPhoneNumber(phoneNumber) {
-  let digits = phoneNumber.replace(/\D/g, '');
+// menu functionality
 
-  let country_code = '';
-  if (digits.length > 11) {
-      country_code = `+${digits.slice(0, digits.length - 11)} `;
-      digits = digits.slice(-11);
-  }
+const elMenuBtn = document.querySelector(".site-header__menu-btn");
 
-  const formattedNumber = country_code + [
-      digits.slice(0, 2),
-      `(${digits.slice(2, 4)})`,
-      digits.slice(4, 6),
-      digits.slice(6, 8),
-      digits.slice(8)
-  ].join(' ');
+const elNavigation = document.querySelector(".site-header__navigation");
 
-  return formattedNumber;
-}
-
-const phoneNumber = "+998904291511";
-const formatted = formatPhoneNumber(phoneNumber);
-console.log(formatted);  // Output: +998 (90) 429 15 11
+elMenuBtn.addEventListener("click", () => {
+  elNavigation.classList.toggle("show");
+  document.body.classList.toggle("unscroll");
+});
